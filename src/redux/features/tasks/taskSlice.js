14a2160beb33e24a34194171e25687cde5e3e6ce
sarker24 <sarker24@gmail.com>
2024-02-19 14:00:55 +0100

    Add task modal make functional

--- src/redux/features/tasks/taskSlice.js
+++ src/redux/features/tasks/taskSlice.js
@@ -6,8 +6,20 @@ const initialState ={
 const taskSlice = createSlice({
     name: 'taskSlice',
     initialState,
-    reducers:{}
+    reducers:{
+        addTask:(state, {payload})=>{
+            if (state.tasks.length === 0) {
+                state.tasks.push({id:1, ...payload})
+            }
+
+            else{
+                const length = state.tasks.length
+                state.tasks.push({id: length+1, ...payload}) 
+            }
+          
+         }
+    }
 })
 
-
+export const {addTask} = taskSlice.actions 
 export default taskSlice.reducer
